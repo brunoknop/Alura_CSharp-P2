d87b96afa4483b091d71fea2702e6e511e508612
Bruno Knop <brunog.knop@gmail.com>
2020-10-05 09:29:40 -0300

    Primeiro commit

---
+++ CSharp-P2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente ContaBruno = new ContaCorrente();
+            ContaBruno.titular = "Bruno Knop";
+            ContaBruno.agencia = 7700;
+            ContaBruno.numero = 000789;
+            ContaBruno.saldo = 123456.78;
+
+            Console.WriteLine("Titular: " + ContaBruno.titular);
+            Console.WriteLine("Agência: " + ContaBruno.agencia);
+            Console.WriteLine("Conta: " + ContaBruno.numero);
+            Console.WriteLine("Saldo: R$ " + ContaBruno.saldo);
+        }
+    }
+}
