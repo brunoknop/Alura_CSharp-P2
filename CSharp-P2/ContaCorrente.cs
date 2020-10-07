2bd259c6f168923cbed4bdb48d53f108133da4de
Bruno Knop <brunog.knop@gmail.com>
2020-10-07 01:46:47 -0300

    Continuação do desenvolvimento

--- CSharp-P2/ContaCorrente.cs
+++ CSharp-P2/ContaCorrente.cs
@@ -1,7 +1,46 @@
-﻿public class ContaCorrente
+﻿using System;
+
+public class ContaCorrente
 {
     public string titular;
     public int agencia;
     public int numero;
     public double saldo;
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente recebedor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.Sacar(valor);
+            recebedor.Depositar(valor);
+            return true;
+        }
+    }
+
+    public void MostraSaque()
+    {
+
+    }
 }
--- CSharp-P2/Program.cs
+++ CSharp-P2/Program.cs
@@ -8,14 +8,22 @@ namespace CSharp_P2
         {
             ContaCorrente ContaBruno = new ContaCorrente();
             ContaBruno.titular = "Bruno Knop";
-            ContaBruno.agencia = 7700;
-            ContaBruno.numero = 000789;
+            ContaBruno.agencia = 987;
+            ContaBruno.numero = 654321;
             ContaBruno.saldo = 123456.78;
 
-            Console.WriteLine("Titular: " + ContaBruno.titular);
-            Console.WriteLine("Agência: " + ContaBruno.agencia);
-            Console.WriteLine("Conta: " + ContaBruno.numero);
-            Console.WriteLine("Saldo: R$ " + ContaBruno.saldo);
+            ContaCorrente ContaToia = new ContaCorrente();
+            ContaToia.titular = "Vitória Kayser Knop";
+            ContaToia.agencia = 987;
+            ContaToia.numero = 654321;
+            ContaToia.saldo = 100;
+
+            ContaBruno.Sacar(654);
+            Console.WriteLine("");
+            ContaBruno.Depositar(789);
+            Console.WriteLine("");
+            ContaBruno.Transferir(4567, ContaToia);
+
         }
     }
 }
